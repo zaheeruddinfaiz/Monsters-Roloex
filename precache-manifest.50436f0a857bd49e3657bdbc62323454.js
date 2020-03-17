5ef0622b1ee25cfa1bb6701bd785429da14dfa3b
zaheeruddinfaiz <zaheerfazy@gmail.com>
2020-03-18 00:18:42 +0500

    Updates

---
+++ precache-manifest.50436f0a857bd49e3657bdbc62323454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b688ac5520d160f8813402750660debd",
+    "url": "/Monsters-Roloex/index.html"
+  },
+  {
+    "revision": "8e360c65482b749417a6",
+    "url": "/Monsters-Roloex/static/css/main.376e77cd.chunk.css"
+  },
+  {
+    "revision": "8a89638b0aea37ea1f45",
+    "url": "/Monsters-Roloex/static/js/2.83e9e59f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters-Roloex/static/js/2.83e9e59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e360c65482b749417a6",
+    "url": "/Monsters-Roloex/static/js/main.e7056f9f.chunk.js"
+  },
+  {
+    "revision": "9f526e64c0c6fb2670cc",
+    "url": "/Monsters-Roloex/static/js/runtime-main.c107aed9.js"
+  }
+]);
